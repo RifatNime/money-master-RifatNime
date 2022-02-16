bb1eee0e99acd0559b4b88c30ba8e852d1e1f6a1
RifatNime <mdrifatnime@gmail.com>
2022-02-17 03:19:20 +0600

    new function added and icons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//
+const amountCalculation = document.getElementById("calculate-btn");
+amountCalculation.addEventListener("click", function(){
+    // console.log("yes");
+    
+    const foodInput = document.getElementById("food-cost").value;
+    const rentInput = document.getElementById("rent-cost").value;
+    const clothInput = document.getElementById("clothes-cost").value;
+    const expencesAmount = parseInt(foodInput) + parseInt(rentInput) + parseInt(clothInput);
+    console.log(expencesAmount);
+    const totalExpensesAmount = document.getElementById("total-expenses");
+    totalExpensesAmount.innerText =  expencesAmount;
+    
+    console.log(totalExpensesAmount);
+    
+
+});
+
